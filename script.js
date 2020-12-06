b01de818c25f125382856fbadf5a42be60eedf03
Aldrian <himalosc@gmail.com>
2020-12-06 20:35:23 +0800

    adding more styles, trying to figure out problem

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".container");
+const box = document.querySelectorAll(".box");
+
+box[0].setAttribute("src","boxes/123743049_646741469342973_7638873703721024418_n.jpg");
+
+
