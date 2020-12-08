84e7da98428af3f6802fcee9253c87610f492ecf
Aldrian <himalosc@gmail.com>
2020-12-08 18:58:11 +0800

    adding features for image

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ const box = document.querySelectorAll(".box");
 /* buttons */
 const btnMenu = document.querySelector(".btnMenu");
 
+
 /* settings */
 const menuFrame = document.querySelector(".menuFrame");
+const btnExit = document.querySelector(".btnExit");
 
-const chckBoxes = () => {
-    
-}
+/* settings inputs */
+const textImg = document.querySelector(".textImg");
+const textURL = document.querySelector(".textURL");
+const imgDiv = document.querySelector(".imgDiv");
+const imgPreview = document.querySelector("#imgPreview");
+
+/* settings */
+/* Open menuFrame, Exit menuFrame */
 
 const menuFrameMagic = () => {
     if(menuFrame.style.display !== "none"){
@@ -19,7 +26,7 @@ const menuFrameMagic = () => {
     }
 }
 
-const clickExit = e => {
+const windowClick = e => {
     if(e.target.className === menuFrame.className){
         menuFrameMagic();
     }else if(e.target.className === "noneyet"){
@@ -29,5 +36,41 @@ const clickExit = e => {
 
 menuFrameMagic();
 
+/* image functions */
+const preventD = e =>{
+    return e.preventDefault();
+}
+
+const imageDrop = e =>{
+    preventD(e);
+    
+    const file = e.dataTransfer.files[0];
+    
+    if(file.type.indexOf("image/")<0){
+        console.log("nope");
+    }else{
+        const reader = new FileReader();
+        
+        reader.onloadend = () => {
+            if(reader.error.message){
+                console.log("error reading file");
+                return;
+            }
+            
+        }
+        
+        reader.readAsBinaryString(file);
+    }
+}
+
+/* Open menuFrame, Exit menuFrame */
 btnMenu.addEventListener("click", menuFrameMagic);
-window.addEventListener("click", clickExit);
+window.addEventListener("click", windowClick);
+btnExit.addEventListener("click", menuFrameMagic);
+
+/* image functions */
+textImg.addEventListener("drop", imageDrop);
+imgDiv.addEventListener("dragleave", preventD);
+imgDiv.addEventListener("dragover", preventD);
+imgDiv.addEventListener("dragenter", preventD);
+imgDiv.addEventListener("drop", imageDrop);
